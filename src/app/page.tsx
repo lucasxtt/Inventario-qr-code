3cfdbd199979552160fd673922f442104b94176a
Lucas Silva <caiolucasalmeida0902@gmail.com>
2025-08-20 16:58:34 -0300

    navbar funcionando, adicionado notf, config e dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      
-      Dashboard
-      
-    </main>
+    <Dashboard />
   );
 }
